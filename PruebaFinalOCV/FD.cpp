fe4778474a47bdf449c31f1d798b162bd77e56b2
danielroa98 <daniel.roa98@gmail.com>
2019-10-07 11:50:53 -0500

    La camara se abre correctamente, falta la detección

--- PruebaFinalOCV/FD.cpp
+++ PruebaFinalOCV/FD.cpp
@@ -3,11 +3,27 @@
 #include<opencv2\objdetect\objdetect.hpp>
 #include<opencv2\imgproc\imgproc.hpp>
 #include<opencv2\highgui\highgui.hpp>
+#include<opencv2\videoio\videoio.hpp>
+#include<opencv2\opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-int main() {
+int main(int argc, char** argv) {
+
+	VideoCapture cap;
+	// open the default camera, use something different from 0 otherwise;
+	// Check VideoCapture documentation.
+	if (!cap.open(0))
+		return 0;
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame;
+		if (frame.empty()) break; // end of video stream
+		imshow("this is you, smile! :)", frame);
+		if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
+	}
 
 	CascadeClassifier faceDetection;
 	if (!faceDetection.load("C:\\OpenCV-4.1.1\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml"))
@@ -15,15 +31,16 @@ int main() {
 		cout << "\nFile isn't loaded properly";
 			exit(0);
 	}
-
+	/*
 	char path[100];
-
 	cout << "\nEnter the path to the image for face detection:";
 	cin.getline(path, 100);
+	*/
 
-	Mat img = imread(path, IMREAD_UNCHANGED);
+	//Mat img = imread(path, IMREAD_UNCHANGED);
 
-	if (img.empty()) {
+
+	if (cap.isOpened()) {
 		cout << "\nNo hay imagen";
 	}
 	else
@@ -31,18 +48,19 @@ int main() {
 		cout << "\nSe encontró la foto\nProcesando";
 	
 		vector<Rect>faces;
-	
-		faceDetection.detectMultiScale(img, faces);
+		Mat vid;
+
+		vector<Rect>rostros;
+
+		faceDetection.detectMultiScale(rostros, faces);
 	
 		for (int i = 0; i < faces.size(); i++)
 		{
 			Point pt1(faces[i].x, faces[i].y);
 			Point pt2((faces[i].x + faces[i].height), (faces[i].y + faces[i].width));
 
-			rectangle(img, pt1, pt2, Scalar(0, 0, 255), 2, 8, 0);
+			rectangle(rostros, pt1, pt2, Scalar(0, 0, 255), 2, 8, 0);
 		}//fin for
-
-		imwrite("C:\\Users\\danie\\Pictures\\Hrvatska\\KRKA\\Demo.jpg", img);
 
 		cout << "\nSe detectaron rostros";
 	}
